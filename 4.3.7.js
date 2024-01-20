736981ff7c9a4e3a1d9a8093bf36beabf220a717
Dmitry Bartoshkin <bartoshkin@gmail.com>
2024-01-20 16:57:44 +0300

    Added function debounce and EventListener for input

--- 4.3.7.js
+++ 4.3.7.js
@@ -1,18 +1,33 @@
-let div = document.createElement('div');
-let input = document.createElement('input');
+let div = document.createElement("div");
+let input = document.createElement("input");
 
 document.body.prepend(div);
 div.append(input);
 
 async function getRepo(str) {
-    str = String(str);
-    let countResult = 5;
-    let url = `https://api.github.com/search/repositories?q=${str}&per_page=${countResult}`;
-    let getResponce = await fetch(url, {
-        headers: {
-            "Accept": "application/vnd.github+json"
-        }
-    })
-    
-    return await getResponce.json();
+  str = String(str);
+  let countResult = 5;
+  let url = `https://api.github.com/search/repositories?q=${str}&per_page=${countResult}`;
+  let getResponce = await fetch(url, {
+    headers: {
+      Accept: "application/vnd.github+json",
+    },
+  });
+
+  return await getResponce.json();
 }
+
+function debounce(fn, time) {
+  let timeout;
+
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn.apply(this, args), time);
+  };
+}
+
+
+input.addEventListener('keypress', debounce(function(e) {
+    console.log(getRepo(e.target.value));
+    // debounce(console.log(e.key), 500);
+}, 500));
